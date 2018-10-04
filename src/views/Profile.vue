3083ddcd0abf235dd3863bbab674c073d530a4a5
KatherineEvans <Evans.Katherine.E@gmail.com>
2018-10-04 11:34:06 -0500

    Added profile bootstrap card, axios GET user profile -incomplete

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,26 +3,17 @@
     <div id="nav">
       <router-link to="/home">Home</router-link> |
       <router-link to="/logout">Logout</router-link>
-      <a href="#/profile"><img class="profile-image" src="../profile_img2.png"></a>
     </div>
-    <div class="itineraryHeader">
-      <h1>{{ trip.itineraries[0].trip_name }}</h1>
+    <div class="profileHeader">
       <h3>{{ message }}</h3>
-      <button type="button" class="btn btn-dark" onClick="window.location = '#/itinerary';" v-model="trip.id">Add Itinerary Item</button>
     </div>
-    <div>
-      <div class="container" id="itineraryList">
-        <div class="row rowBackground">
-          <div>
-            <ul class="timeline">
-              <li v-for=" itinerary in trip.itineraries" v-on:click="toggleShowImage(itinerary)">
-                <p class="bold">{{ itinerary.description }}</p>
-                <p class="italics small">{{ itinerary.time | moment("dddd, MMMM Do YYYY, h:mm a") }}</p>
-                <img class="itineraryPhoto" v-bind:src="itinerary.photo" height="200" v-if="itinerary.show_image">
-              </li>
-            </ul>
-          </div>
-        </div>
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title" v-for="current_user in profile">{{ current_user.name }}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
       </div>
     </div>
   </div>
@@ -37,30 +28,19 @@ var axios = require("axios");
 export default {
   data: function() {
     return {
-      message: "Trip Itinerary",
+      message: "Your Profile",
+      users: [],
       trips: [],
-      trip: {itineraries: [{trip_name: "Loading..."}]},
-      ininerary: {},
-      itineraries: []
+      profile: this.profile,
     };
   },
   created: function() {
-    axios
-      .get(
-        "http://localhost:3000/api/trips/" +
-          this.$route.params.id +
-          "/itinerary"
-      )
-      .then(response => {
-        console.log("itineraries", response.data.itineraries);
-        this.trip = response.data;
-      });
+    axios.get("http://localhost:3000/api/travelers/1").then(response => {
+      console.log("User Profile", response.data);
+      this.profile = response.data;
+    });
   },
-  methods: {
-    toggleShowImage: function(itinerary) {
-      itinerary.show_image = !itinerary.show_image;
-    }
-  },
+  methods: {},
   computed: {}
 };
 </script>
